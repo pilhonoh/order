96f4643ddc3fe47cc055bdc1019f7728e8098611
eric <eric93@nate.com>
2020-08-21 14:48:22 +0900

    recommit3

---
+++ src/main/java/local0811/OrderRepository.java
@@ -0,0 +1,8 @@
+package local0811;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface OrderRepository extends PagingAndSortingRepository<Order, Long>{
+
+
+}
---
+++ src/main/java/local0811/Ordered.java
@@ -0,0 +1,39 @@
+package local0811;
+
+public class Ordered extends AbstractEvent {
+
+    private Long id;
+    private Long ProductID;
+    private Integer qty;
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public Long getProductId() {
+        return ProductID;
+    }
+
+    public void setProductId(Long ProductID) {
+        this.ProductID = ProductID;
+    }
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
